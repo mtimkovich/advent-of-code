12b78710816aac66ffdaa34734fd1523e276d847
Max Timkovich <mtimkovich@gmail.com>
2020-12-13 19:15:40 -0700

    day3 part 2

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,6 @@ fn main() {
         .expect("filename is required");
     let path = format!("../inputs/{}", filename);
     let inputs = read_lines(&path).unwrap();
-    let result = day3::2art2(&inputs);
+    let result = day3::part2(&inputs);
     println!("{}", result);
 }
