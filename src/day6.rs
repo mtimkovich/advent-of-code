5406543070f92637739bd51dd5e17a0243a2ed44
Max Timkovich <mtimkovich@gmail.com>
2020-12-14 17:11:08 -0700

    day 6.2

---
+++ src/day6.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+pub fn part1(lines: &Vec<String>) -> usize {
+    let mut answers = HashSet::new();
+    let mut sum = 0;
+
+    for line in lines {
+        if line == "" {
+            sum += answers.len();
+            answers.clear();
+            continue;
+        }
+
+        for c in line.chars() {
+            answers.insert(c);
+        }
+    }
+
+    sum
+}
+
+pub fn part2(lines: &Vec<String>) -> usize {
+    let mut answers: HashMap<char, u32> = HashMap::new();
+    let mut sum = 0;
+    let mut members = 0;
+
+    for line in lines {
+        if line == "" {
+            let everyone = answers.iter()
+                                  .filter(|(_, &v)| v == members)
+                                  .count();
+            sum += everyone;
+            answers.clear();
+            members = 0;
+            continue;
+        }
+
+        for c in line.chars() {
+            if !answers.contains_key(&c) {
+                answers.insert(c, 0);
+            }
+
+            answers.insert(c, *answers.get(&c).unwrap() + 1);
+        }
+        members += 1;
+    }
+
+    sum
+}
